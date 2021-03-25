10c96f1ea8614d1d1afc401824f036c7a94b92db
Lance Depew <Metal_Death_Music@yahoo.com>
2021-03-25 14:16:00 -0400

    first connect to back end user

--- js/user-list.js
+++ js/user-list.js
@@ -1,4 +1,15 @@
+const url = "http://localhost:8080/api/users/";
+
 $().ready(() => {
 
     console.log("jQuery is ready!")
+
+    $.getJSON(url)
+        .done((users) => {
+            console.log(users);
+        })
+        .fail((err) => {
+            console.error(err);
+        })
+
 });
