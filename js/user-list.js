994adc5a9cdba5b487aee13fccaae7704dcc3625
Lance Depew <Metal_Death_Music@yahoo.com>
2021-03-25 14:45:24 -0400

    add new user

--- js/user-list.js
+++ js/user-list.js
@@ -1,15 +1,40 @@
 const url = "http://localhost:8080/api/users/";
 
+let newuser = {
+    id: 0, 
+    username: "bus", 
+    password: "bus", 
+    firstName: "Nbormal", 
+    lastName: "Ubser",
+    phoneNumber: "555-555-1212",
+    email: "binfo@user.com",
+    isReviewer: false,
+    isAdmin: false
+}
+
 $().ready(() => {
 
-    console.log("jQuery is ready!")
+    $.ajax({
+        method: "POST",
+        url: url,
+        data: JSON.stringify(newuser),
+        contentType: 'application/json'
+    })
+    .done((res) => {
+        console.log(res);
+    })
+    .fail((err) => {
+        console.error(err);
+    })
 
-    $.getJSON(url)
-        .done((users) => {
-            console.log(users);
-        })
-        .fail((err) => {
-            console.error(err);
-        })
+
+
+    // $.getJSON(url)
+    //     .done((users) => {
+    //         console.log(users);
+    //     })
+    //     .fail((err) => {
+    //         console.error(err);
+    //     })
 
 });
