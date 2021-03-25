1d964f6a156ed95bbb9dd5c98ae65000e48d5fe3
Lance Depew <Metal_Death_Music@yahoo.com>
2021-03-25 14:48:44 -0400

    only 2 calls

--- js/user-list.js
+++ js/user-list.js
@@ -2,39 +2,39 @@ const url = "http://localhost:8080/api/users/";
 
 let newuser = {
     id: 0, 
-    username: "bus", 
-    password: "bus", 
-    firstName: "Nbormal", 
-    lastName: "Ubser",
+    username: "us", 
+    password: "us", 
+    firstName: "Normal", 
+    lastName: "User",
     phoneNumber: "555-555-1212",
-    email: "binfo@user.com",
+    email: "info@user.com",
     isReviewer: false,
     isAdmin: false
 }
 
 $().ready(() => {
 
-    $.ajax({
-        method: "POST",
-        url: url,
-        data: JSON.stringify(newuser),
-        contentType: 'application/json'
-    })
-    .done((res) => {
-        console.log(res);
-    })
-    .fail((err) => {
-        console.error(err);
-    })
+    // $.ajax({
+    //     method: "POST",
+    //     url: url,
+    //     data: JSON.stringify(newuser),
+    //     contentType: 'application/json'
+    // })
+    // .done((res) => {
+    //     console.log(res);
+    // })
+    // .fail((err) => {
+    //     console.error(err);
+    // })
 
 
 
-    // $.getJSON(url)
-    //     .done((users) => {
-    //         console.log(users);
-    //     })
-    //     .fail((err) => {
-    //         console.error(err);
-    //     })
+    $.getJSON(url)
+        .done((users) => {
+            console.log(users);
+        })
+        .fail((err) => {
+            console.error(err);
+        })
 
 });
